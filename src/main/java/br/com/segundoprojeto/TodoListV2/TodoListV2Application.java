73ace5b1fd406f895944ddf67326932770b8fc33
Thelozx <talisfilipe74@outlook.com>
2023-10-23 16:06:33 -0300

    Initial projectmvn package

---
+++ src/main/java/br/com/segundoprojeto/TodoListV2/TodoListV2Application.java
@@ -0,0 +1,13 @@
+package br.com.segundoprojeto.TodoListV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListV2Application.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/segundoprojeto/TodoListV2/controller/MinhaPrimeiraController.java
@@ -0,0 +1,14 @@
+package br.com.segundoprojeto.TodoListV2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class MinhaPrimeiraController {
+
+    public String primeiraMensagem()
+    
+}
+}
+ 
