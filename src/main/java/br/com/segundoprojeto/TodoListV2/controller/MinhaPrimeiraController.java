0ba0d21307b71227dccdc5bbffaa86a061acdfb5
Thelozx <talisfilipe74@outlook.com>
2023-10-23 16:10:00 -0300

    Initial project

--- src/main/java/br/com/segundoprojeto/TodoListV2/controller/MinhaPrimeiraController.java
+++ src/main/java/br/com/segundoprojeto/TodoListV2/controller/MinhaPrimeiraController.java
@@ -1,14 +1,12 @@
 package br.com.segundoprojeto.TodoListV2.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class MinhaPrimeiraController {
 
-    public String primeiraMensagem()
-    
+    public String primeiraMensagem(){
+        return "sdkfsjfkls";
+    }
 }
-}
- 
